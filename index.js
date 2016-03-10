0855603f1cf0dead7ecc72691c77563106b20ce2
Andrew Duthie <andrew@andrewduthie.com>
2016-03-10 17:50:55 -0500

    Remove environment check

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 module.exports = function() {
 	var context = this.query.substr( 1 );
-
-	if ( process.env.NODE_ENV !== 'production' ) {
-		return 'module.exports = require( \'debug\' )( \'' + context + '\' );';
-	}
-
-	return 'module.exports = function() {};'
+	return 'module.exports = require( \'debug\' )( \'' + context + '\' );';
 }
